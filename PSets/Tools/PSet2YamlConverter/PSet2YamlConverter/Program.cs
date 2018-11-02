c35bf00060917392ccf6077647224e284aaa0dba
Klaus Aengenvoort <klaus.aengenvoort@etask.de>
2018-11-02 07:53:18 +0100

    Maintain the official PSets and their localizations #2 Transfer of the PSets and the related Tools from https://github.com/klacol/PSets to here

---
+++ PSets/Tools/PSet2YamlConverter/PSet2YamlConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace PSet2YamlConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"PSet Manager started");
+            Console.WriteLine($"A tool by the community of buildingSMART International");
+            Console.WriteLine($"The home of PSet Manager is https://github.com/buildingsmart/bSDD");
+
+            string sourceFolderXml = args[0];
+            string targetFolderYaml = args[1];
+            string targetFolderJson = args[2];
+            Console.WriteLine($"Converting the PSets from this source folder: {sourceFolderXml}");
+            Console.WriteLine($"Into YAML files in this target folder: {targetFolderYaml}");
+            Console.WriteLine($"Into JSON files in this target folder: {targetFolderJson}");
+
+            Converter converter = new Converter(sourceFolderXml, targetFolderYaml, targetFolderJson, true);
+
+            Console.WriteLine($"Successfully finished - Be happy with your Open BIM");
+        }
+    }
+}
