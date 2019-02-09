05b0f15424faea1dee23cf19469b7d5817ad859b
Klaus Aengenvoort <klaus.aengenvoort@etask.de>
2019-02-09 15:35:22 +0100

    Transfer the translation for de-DE into this repository and upstream it into bSDD and on http://www.buildingsmart-tech.org #7

--- PSets/Tools/ConvertAndPublishPSets.ps1
+++ PSets/Tools/ConvertAndPublishPSets.ps1
@@ -1,25 +1,38 @@
+param(
+[string]$bsddUrl,
+[string]$bsddUser,
+[string]$bsddPassword
+)
+
+
 $BaseFolder = "C:\projects\bSDD\PSets"
 $PSetManagerExe="$BaseFolder\Tools\PSetManager\PSetManager\bin\Release\PSetManager.exe"
-$SourceFolderXml = "$BaseFolder\XML"
-$TargetFolderYaml = "$BaseFolder\YAML"
-$TargetFolderJson = "$BaseFolder\JSON"
-$TargetFolderResx = "$BaseFolder\RESX"
+$FolderXml = "$BaseFolder\XML"
+$FolderYaml = "$BaseFolder\YAML"
+$FolderJson = "$BaseFolder\JSON"
+$FolderResx = "$BaseFolder\RESX"
 $ExitCode=0
 Write-Host "++++++++++++++++++++++++++++++++++++++"
-Write-Host SourceFolderXml : $SourceFolderXml
+Write-Host SourceFolderXml  : $SourceFolderXml
 Write-Host TargetFolderYaml : $TargetFolderYaml
 Write-Host TargetFolderJson : $TargetFolderJson
-Write-Host TargetFolderResx : $TargetFolderResx
+Write-Host bsddUrl          : $bsddUrl
 
-& $PSetManagerExe --mode ConvertFromXml --folderXml $SourceFolderXml --folderYaml $TargetFolderYaml --folderJson $TargetFolderJson --folderResx $TargetFolderResx --checkBSDD true
+## Run the PSetManager in the mode to convert the XML-Psets to the new YAML format
+## This mode was only created for the initial conversion of the PSets
+# & $PSetManagerExe --mode ConvertFromXml --folderXml $FolderXml --folderYaml $FolderYaml --folderJson $tFolderJson --folderResx $FolderResx --checkBSDD true
+
+## Run the PSetManager in the mode to publish them from the new YAML format to the target bSDD server
+& $PSetManagerExe --mode PublishToBSDD --folderYaml $FolderYaml --bsddLanguageCode "de-DE" --bsddUrl $bsddUrl --bsddUser $bsddUser --bsddPassword $bsddPassword 
+
 
 if ($LastExitCode -eq '0') 
 { 
- Write-Host "OK: Transformation is successfull" -ForegroundColor Green
+ Write-Host "OK: Publication was successfull" -ForegroundColor Green
 }
 else 
 {
- Write-Host "ERROR: Transformation is not successfull" -ForegroundColor Red
+ Write-Host "ERROR: Publication was not successfull" -ForegroundColor Red
  Write-Host "Please check the errors and correct them. Thanks for your contribution!" -ForegroundColor Red
  $ExitCode=1
 }
