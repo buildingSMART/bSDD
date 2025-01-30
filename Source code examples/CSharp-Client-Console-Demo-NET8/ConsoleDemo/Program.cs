b62739f44b99d93d69ec88810c17c050b3699f5c
Baars-IT <erik@baarsit.nl>
2025-01-30 20:01:25 +0100

    Remove some unneeded lines

--- Source code examples/CSharp-Client-Console-Demo-NET8/ConsoleDemo/Program.cs
+++ Source code examples/CSharp-Client-Console-Demo-NET8/ConsoleDemo/Program.cs
@@ -6,13 +6,9 @@ using Microsoft.Identity.Client;
 
 var config = new PublicClientApplicationOptions
 {
-    // 'Directory (tenant) ID' of the app registration in the Microsoft Entra admin center
-    TenantId = "14762077-e0ef-404b-b843-5c32a95a0d43",
-
     // 'Application (client) ID' of the app registration in the Microsoft Entra admin center
     ClientId = "4aba821f-d4ff-498b-a462-c2837dbbba70"
 };
-
 
 const string tenantName = "buildingsmartservices";
 const string tenant = $"{tenantName}.onmicrosoft.com";
@@ -65,12 +61,8 @@ if (msalAuthenticationResult == null)
         .ExecuteAsync();
 }
 
-// At this point we now have a valid access token for Microsoft Graph, with only the specific scopes
-// necessary to complete the following call. Build the Microsoft Graph HTTP request, using the obtained
-// access token.
 using var searchRequest = new HttpRequestMessage(HttpMethod.Get, searchListUrl);
 searchRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", msalAuthenticationResult.AccessToken);
-//searchRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJSUzI1NiIsImtpZCI6InZ6bTcxb1o0WjZlQVRYdC0zUmF2SUEybUdnVG8xeDJpZmgyX0FGZk5oMEEiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiIxNDRlMDM3Zi02YTcxLTQxYmItOGNlMS00ZTg1NTQ1MjA3NTYiLCJpc3MiOiJodHRwczovL2F1dGhlbnRpY2F0aW9uLmJ1aWxkaW5nc21hcnQub3JnLzE0NzYyMDc3LWUwZWYtNDA0Yi1iODQzLTVjMzJhOTVhMGQ0My92Mi4wLyIsImV4cCI6MTczMDI4NDgyMywibmJmIjoxNzMwMjgxMjIzLCJzdWIiOiI4N2FmNmMxOC03ZTYyLTQzZWEtYTgzMi1iZWY0OWIwMzBhZWMiLCJlbWFpbCI6ImVyaWtfYmFhcnNAaG90bWFpbC5jb20iLCJuYW1lIjoiRXJpayBCLiIsImdpdmVuX25hbWUiOiJFcmlrIiwiZmFtaWx5X25hbWUiOiJCYWFycyIsImlzRm9yZ290UGFzc3dvcmQiOmZhbHNlLCJub25jZSI6IjFjYWM4NWNlLTAzNDItNGJkNi1iYjE4LTUzMGM1MTAzNTA5MiIsInNjcCI6Im1hbmFnZSByZWFkIiwiYXpwIjoiZjYwY2MzZGUtYzBhNC00NjI3LTlmZjktNWUxNGE1OWQxYWNlIiwidmVyIjoiMS4wIiwiaWF0IjoxNzMwMjgxMjIzfQ.r-ox5LV66iiAz5GeGwL7cm5OGMhV1rB_4_Ai2HIKni3i8TAOknx6yqDQFJMT7hHhZJSrtOU8_CDTbcKFL56Fq68NbcnHVoxqIOhM_I-IY0ulxN2SnrMgOJ7GlGFQWPonbclTzL_RspAIwyMHv38Df4CWlbE4dsoeqFXq4ait8yCwBGpma3-awuum2-CpoVEZjhTXnhPMh8z2nZhiiUYobT9Qru_RObo53Rzo4nmqRaN8VyiLysVJIrwu2habmX5YjJGGtUUDZcrjTg8skrCBWwLP3WLB5sT6KW4LnvFauX6I3X42EL-eD3BKoF2AWaDsWAUECGxJgl8_HKdzT76ODg");
 
 // Make the API call
 var httpClient = new HttpClient();
